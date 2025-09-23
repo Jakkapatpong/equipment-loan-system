618f6e27d38fb61017b669b273390db654827b1b
Jakkapatpong <95201744+Jakkapatpong@users.noreply.github.com>
2025-09-24 00:32:33 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 async function api(action, payload = {}) {
-  const form = new URLSearchParams();
-  form.append("data", JSON.stringify({ action, token: getToken(), payload }));
+  const body = JSON.stringify({ action, token: getToken && getToken(), payload });
   const res = await fetch(API_BASE, {
     method: "POST",
-    body: form // simple request เช่นกัน
+    body
   });
   return res.json();
 }
